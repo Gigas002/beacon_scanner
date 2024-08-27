d5500f35e31ba4ce4c3218e9799002cd972c8267
gigas002 <gigas002@pm.me>
2024-08-27 08:02:09 +0000

    Fix permissions for android

--- android/src/main/kotlin/com/plugin/beacon_scanner/Utils.kt
+++ android/src/main/kotlin/com/plugin/beacon_scanner/Utils.kt
@@ -18,11 +18,13 @@ internal object Utils {
     fun requestPermissions(activityPluginBinding: ActivityPluginBinding) {
         ActivityCompat.requestPermissions(
             activityPluginBinding.activity, arrayOf(
-                Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.BLUETOOTH,
+                Manifest.permission.BLUETOOTH_ADMIN,
                 Manifest.permission.BLUETOOTH_SCAN,
                 Manifest.permission.BLUETOOTH_CONNECT,
-                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                Manifest.permission.FOREGROUND_SERVICE,
             ), 1234
         )
     }
